bac5a81d89cce1c8ecb75e65417cb1ce71ac30d8
Jediael Mendoza <mend0214@algonquinlive.com>
2025-12-07 09:23:52 -0500

    Change nav bar to blue

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: green;
+  background-color: #0046BE;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
